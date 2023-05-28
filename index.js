c1d88338a0e375cca31648836e1587dac313f1a8
nbcnvc <nbcnvc.dev@gmail.com>
2023-05-29 00:40:59 +0900

    Implement searching

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const baseUrl = 'https://api.themoviedb.org/3/movie/top_rated';
+const imageBaseUrl = 'https://image.tmdb.org/t/p/w200';
 const options = {
 	method: 'GET',
 	headers: {
@@ -6,6 +7,8 @@ const options = {
 			'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJiNmI1OTZjMmY3NDAzNDJlY2VhNTc3MzRmYmRkMzk1NSIsInN1YiI6IjY0NzFlYTA0OTQwOGVjMDEwMDI1MWNmYSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ._ops8vO4n2RQ40MWbUXo64hwKvh8EFlI6t_beidcYdA',
 	},
 };
+let movies;
+const moviesTag = document.querySelector('.movies');
 
 let getFetchData = async (url, options) => {
 	let res = await fetch(url, options);
@@ -16,8 +19,9 @@ let getFetchData = async (url, options) => {
 
 let getMovies = async () => {
 	let url = `${baseUrl}`;
+	movies = await getFetchData(url, options);
 
-	return await getFetchData(url, options);
+	return movies;
 };
 
 let onClickMovie = (e) => {
@@ -25,9 +29,23 @@ let onClickMovie = (e) => {
 	alert(`영화 id: ${id}`);
 };
 
-// entrypoint
-getMovies().then((data) => {
-	for (const movie of data) {
+let handleSubmit = (e) => {
+	e.preventDefault();
+	const inputValue = document.querySelector('.search-box input').value;
+
+	if (inputValue) {
+		const filteredMovies = movies.filter((movie) => {
+			if (movie.title.includes(inputValue)) {
+				return movie;
+			}
+		});
+		moviesTag.innerHTML = '';
+		drawMovies(filteredMovies);
+	}
+};
+
+let drawMovies = (movies) => {
+	for (const movie of movies) {
 		const { id, title, poster_path, vote_average, overview } = movie;
 
 		const card = document.createElement('div');
@@ -37,7 +55,7 @@ getMovies().then((data) => {
 
 		const image = document.createElement('img');
 		image.className = 'movie__img';
-		image.src = `https://image.tmdb.org/t/p/w200${poster_path}`;
+		image.src = `${imageBaseUrl}${poster_path}`;
 		card.appendChild(image);
 
 		const movieInfo = document.createElement('div');
@@ -60,6 +78,10 @@ getMovies().then((data) => {
 		movieInfo.appendChild(movieOverview);
 
 		document.querySelector('.movies').appendChild(card);
-		// break;
 	}
+};
+
+// entrypoint
+getMovies().then((data) => {
+	drawMovies(data);
 });
